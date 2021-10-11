89a050e894d0892d4fc9b3daa5b0ee39cda31fb8
Alberto <aldo10012@gmail.com>
2021-10-11 09:23:21 -0700

    first commit

---
+++ NBA-Stats/NBA_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_StatsApp.swift
+//  NBA-Stats
+//
+//  Created by Alberto Dominguez on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct NBA_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
