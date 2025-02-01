dbec954384240bb018d1a19d829fd66b73ef221b
Alberto <aldo10012@gmail.com>
2025-02-01 13:33:12 -0800

    integrate EZNetworking sdk into APIClient

--- NBA-Stats/Networking/APIClient.swift
+++ NBA-Stats/Networking/APIClient.swift
@@ -6,178 +6,136 @@
 //
 
 import Foundation
+import EZNetworking
+
+enum Endpoint: String {
+    case gamesByDateBaseURL = "https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/"
+    case standingsBaseURL = "https://api.sportsdata.io/v3/nba/scores/json/Standings/"
+    case allTeamsBaseURL = "https://api.sportsdata.io/nba/v2/json/AllTeams"
+    case allPlayersBaseURL = "https://api.sportsdata.io/nba/v2/json/Players/"
+}
 
 class APIClient {
-    let gamesByDateBaseURL = "https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/"
-    let standingsBaseURL = "https://api.sportsdata.io/v3/nba/scores/json/Standings/"
-    let allTeamsBaseURL = "https://api.sportsdata.io/nba/v2/json/AllTeams"
-    let allPlayersBaseURL = "https://api.sportsdata.io/nba/v2/json/Players/"
     let apiKey = "fe9f2cb9842e40ec8c761e78ecc2c58f"
-    let urlSession = URLSession.shared
+    
+    let performer = RequestPerformer()
     
     //MARK: - GamesByDate
 
     /// Get games by date: You can enter "yesterday", "today", or "tomorrow"
     func getGamesByDate(_ date: String, completion: @escaping (Result<[String: [GameByDate] ]>) -> () ) {
-        let fullURL = setGamesByDateParams(date: date)
-        guard let url = URL(string: fullURL) else {return}
-        let request = URLRequest(url: url)
+        let request = RequestFactoryImpl()
+            .build(httpMethod: .GET,
+                   baseUrlString: "\(Endpoint.gamesByDateBaseURL.rawValue)\(date)",
+                   parameters: [.init(key: "key", value: apiKey)]
+            )
         
-        urlSession.dataTask(with: request) { data, responce, error in
-            if let error = error {
-                return completion(Result.failure(error))
-            }
-            
-            guard let data = data else {
-                return completion(Result.failure(EndPointError.noData))
-            }
-            
-            do {
-                let games = try JSONDecoder().decode([GameByDate].self, from: data)
-                
+        performer.performTask(request: request, decodeTo: [GameByDate].self) { result in
+            switch result {
+            case .success(let games):
+                var foobar = [String: [GameByDate] ]()
+                switch date {
+                case yesterday: foobar["yesterday"] = games
+                case today:     foobar["today"]     = games
+                case tomorrow:  foobar["tomorrow"]  = games
+                default:
+                    print("error")
+                }
                 DispatchQueue.main.sync {
-                    var foobar = [String: [GameByDate] ]()
-                    
-                    switch date {
-                    case yesterday: foobar["yesterday"] = games
-                    case today:     foobar["today"]     = games
-                    case tomorrow:  foobar["tomorrow"]  = games
-                    default:
-                        print("error")
-                    }
-                    
                     completion(Result.success(foobar))
                 }
-                
-            } catch {
-                completion(Result.failure(EndPointError.couldNotParse))
-                print(error)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print("DEBUG: ", error)
+                    completion(Result.failure(error))
+                }
             }
-        }.resume()
+        }
     }
-    
-    
-    func setGamesByDateParams(date: String) -> String {
-        return "\(gamesByDateBaseURL)\(date)?key=\(apiKey)"
-    }
-    
     
     // MARK: - Standings
     
     /// Get games by date: You can enter "yesterday", "today", or "tomorrow"
     func getStandings(_ year: Int, completion: @escaping (Result<[String: [Standing]]>) -> () ) {
-        let fullURL = setStandingsParams(year: year)
-        guard let url = URL(string: fullURL) else {return}
-        let request = URLRequest(url: url)
+        let request = RequestFactoryImpl()
+            .build(httpMethod: .GET,
+                   baseUrlString: "\(Endpoint.standingsBaseURL.rawValue)\(year)",
+                   parameters: [.init(key: "key", value: apiKey)]
+            )
         
-        urlSession.dataTask(with: request) { data, responce, error in
-            if let error = error {
-                return completion(Result.failure(error))
-            }
-            
-            guard let data = data else {
-                return completion(Result.failure(EndPointError.noData))
-            }
-            
-            do {
-                let allStandings = try JSONDecoder().decode([Standing].self, from: data)
+        performer.performTask(request: request, decodeTo: [Standing].self) { result in
+            switch result {
+            case .success(let allStandings):
+                var foobar: [String: [Standing]] = [
+                    "easternStandings": [Standing](),
+                    "westernStandings": [Standing]()
+                ]
                 
-                DispatchQueue.main.sync {
-                    var foobar: [String: [Standing]] = [
-                        "easternStandings": [Standing](),
-                        "westernStandings": [Standing]()
-                    ]
-                    
-                    for standing in allStandings {
-                        if standing.conference == "Eastern" {
-                            foobar["easternStandings"]!.append(standing)
-                        } else {
-                            foobar["westernStandings"]!.append(standing)
-                        }
+                for standing in allStandings {
+                    if standing.conference == "Eastern" {
+                        foobar["easternStandings"]!.append(standing)
+                    } else {
+                        foobar["westernStandings"]!.append(standing)
                     }
-                    
-                    completion(Result.success(foobar))
                 }
                 
-            } catch {
-                completion(Result.failure(EndPointError.couldNotParse))
-                print(error)
+                DispatchQueue.main.sync {
+                    completion(Result.success(foobar))
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print("DEBUG: ", error)
+                    completion(Result.failure(error))
+                }
             }
-        }.resume()
+        }
     }
-    
-    
-    func setStandingsParams(year: Int) -> String {
-        return "\(standingsBaseURL)\(year)?key=\(apiKey)"
-    }
-    
     
     // MARK: - All teams
     
     func getAllTeams(completion: @escaping (Result<[Team]>) -> () ) {
-        let fullURL = setAllTeamsParams()
-        guard let url = URL(string: fullURL) else {return}
-        let request = URLRequest(url: url)
+        let request = RequestFactoryImpl()
+            .build(httpMethod: .GET,
+                   baseUrlString: "\(Endpoint.allTeamsBaseURL.rawValue)",
+                   parameters: [.init(key: "key", value: apiKey)]
+            )
         
-        urlSession.dataTask(with: request) { data, responce, error in
-            if let error = error {
-                return completion(Result.failure(error))
-            }
-            
-            guard let data = data else {
-                return completion(Result.failure(EndPointError.noData))
-            }
-            
-            do {
-                let allTeams = try JSONDecoder().decode([Team].self, from: data)
-                
+        performer.performTask(request: request, decodeTo: [Team].self) { result in
+            switch result {
+            case .success(let allTeams):
                 DispatchQueue.main.sync {
                     completion(Result.success(allTeams))
                 }
-                
-            } catch {
-                completion(Result.failure(EndPointError.couldNotParse))
-                print(error)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print("DEBUG: ", error)
+                    completion(Result.failure(error))
+                }
             }
-        }.resume()
+        }
     }
-    
-    func setAllTeamsParams() -> String {
-        return "\(allTeamsBaseURL)?key=\(apiKey)"
-    }
-    
     
     // MARK: - Players
     
     func getAllPlayers(from team: String, completion: @escaping (Result<[Player]>) -> () ) {
-        let fullURL = setAllPlayersParams(teamKey: team)
-        guard let url = URL(string: fullURL) else {return}
-        let request = URLRequest(url: url)
+        let request = RequestFactoryImpl()
+            .build(httpMethod: .GET,
+                   baseUrlString: "\(Endpoint.allPlayersBaseURL.rawValue)\(team)",
+                   parameters: [.init(key: "key", value: apiKey)]
+            )
         
-        urlSession.dataTask(with: request) { data, responce, error in
-            if let error = error {
-                return completion(Result.failure(error))
-            }
-            
-            guard let data = data else {
-                return completion(Result.failure(EndPointError.noData))
-            }
-            
-            do {
-                let allPlayers = try JSONDecoder().decode([Player].self, from: data)
-                
+        performer.performTask(request: request, decodeTo: [Player].self) { result in
+            switch result {
+            case .success(let allPlayers):
                 DispatchQueue.main.sync {
                     completion(Result.success(allPlayers))
                 }
-                
-            } catch {
-                completion(Result.failure(EndPointError.couldNotParse))
-                print(error)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print("DEBUG: ", error)
+                    completion(Result.failure(error))
+                }
             }
-        }.resume()
-    }
-    
-    func setAllPlayersParams(teamKey: String) -> String {
-        return "\(allPlayersBaseURL)\(teamKey)?key=\(apiKey)"
+        }
     }
 }
